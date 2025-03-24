ff1e9439858a2067abb639c834302f8ca659433c
rharbi <iirharbi@gmail.com>
2025-03-24 03:24:03 +0300

    Implement ExpenseResource

---
+++ src/main/java/com/rayan/salarytracker/resource/ExpenseResource.java
@@ -0,0 +1,50 @@
+package com.rayan.salarytracker.resource;
+
+import com.rayan.salarytracker.core.exception.EntityNotFoundException;
+import com.rayan.salarytracker.dto.expense.ExpenseInsertDTO;
+import com.rayan.salarytracker.dto.expense.ExpenseReadOnlyDTO;
+
+import com.rayan.salarytracker.service.impl.ExpenseService;
+import io.quarkus.security.Authenticated;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+import java.util.List;
+
+@Path(("/expenses"))
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Authenticated
+public class ExpenseResource {
+
+    @Inject
+    ExpenseService expenseService;
+
+    @GET
+    @Path("/{salaryId}")
+    public Response getExpenses(@PathParam("salaryId") Long salaryId) {
+        List<ExpenseReadOnlyDTO> expenseReadOnlyDTO = expenseService.getAllExpenses(salaryId);
+        return Response.status(Response.Status.OK).entity(expenseReadOnlyDTO).build();
+    }
+
+    @POST
+    @Path("/{id}")
+    public Response addExpense(@PathParam("id")Long salaryId, ExpenseInsertDTO expenseInsertDTO) throws EntityNotFoundException {
+        ExpenseReadOnlyDTO expenseReadOnlyDTO = expenseService.createExpense(expenseInsertDTO, salaryId);
+        System.out.println(expenseInsertDTO + " ********");
+        return Response.status(Response.Status.CREATED)
+                .entity(expenseReadOnlyDTO)
+                .build();
+    }
+
+    @GET
+    @Path("/{salaryId}/{expenseId}")
+    public Response getExpense(@PathParam("expenseId") Long expenseId) throws EntityNotFoundException {
+        ExpenseReadOnlyDTO expenseReadOnlyDTO = expenseService.getExpenseById(expenseId);
+        return Response.status(Response.Status.OK).entity(expenseReadOnlyDTO).build();
+    }
+}
+
